1309e23a371846bf6408c225e33e88df0bcd90c6
mcbrialy <mcb22002@byui.edu>
2025-01-11 22:50:57 -0700

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int percent = int.Parse(input);
+
+        string letter = "";
+        string pass = "You passed!";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+
+        else if (percent <60)
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your letter grade is {letter}.");
+        
+        if (percent < 70)
+        {
+            pass = "You failed.";
+        }
+        Console.WriteLine($"{pass}");
     }
 }
