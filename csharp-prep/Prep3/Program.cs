1be70a0042834e9c9716f5489eaaa025e59ece7a
mcbrialy <mcb22002@byui.edu>
2025-01-16 09:02:03 -0700

    prep2 and prep3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,32 @@
 using System;
-
+using System.Diagnostics;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_num = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        
+        while (guess != magic_num)
+        {
+            Console.Write("Input your guess: ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+
+            if (guess > magic_num)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magic_num)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
